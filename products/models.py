63b9d7a9307c473cf3fcb0d9438cc18ed67fa664
ulawanat <wanatula@gmail.com>
2021-12-07 22:57:51 +0100

    Polish changes

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
         return self.title
     
     def summary(self):
-        return self.body[:100]
+        return self.body[:100] + '...'
     
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
